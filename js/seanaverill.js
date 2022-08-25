691fb924f181f2c95e4356b2567d5a73c2633e43
saverill24 <saverill24@gmail.com>
2022-08-24 22:03:39 -0400

    JS Updates

--- js/seanaverill.js
+++ js/seanaverill.js
@@ -1,4 +1,35 @@
+
+// Dark Mode Code
 function myFunction() {
     var element = document.body;
     element.classList.toggle("light");
     }
+
+
+// Accordion Code
+$('.panel-collapse').on('show.bs.collapse', function () {
+    $(this).siblings('.panel-heading').addClass('active');
+  });
+
+  $('.panel-collapse').on('hide.bs.collapse', function () {
+    $(this).siblings('.panel-heading').removeClass('active');
+  });
+        
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {stickyFunc()};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyFunc() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
